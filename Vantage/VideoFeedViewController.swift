ffbdd187624731b1a1eb94a215d8b5a99abc1f5e
Lauren N. Roth <laurenroth@gmail.com>
2015-04-18 17:59:52 -0400

    Query parse, return objects to the table view controller. 	modified:   Vantage.xcodeproj/project.pbxproj 	modified:   Vantage/Base.lproj/Main.storyboard 	new file:   Vantage/TestVideoFeedController.swift 	modified:   Vantage/VideoFeedViewController.swift

--- Vantage/VideoFeedViewController.swift
+++ Vantage/VideoFeedViewController.swift
@@ -9,21 +9,82 @@
 import UIKit
 import Parse
 
-class VideoFeedViewController: UIViewController {
+class VideoFeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource  {
+
+    @IBOutlet weak var tableView: UITableView!
+    var movieArray = [];
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let testObject = PFObject(className: "TestObject")
-        testObject["foo"] = "baringo"
-        testObject.saveInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
-            println("Object has been saved.")
-        }
+        tableView.dataSource = self;
+        tableView.delegate = self;
+        
+//        let testObject = PFObject(className: "TestObject")
+//        testObject["foo"] = "baringo"
+//        testObject.saveInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
+//            println("Object has been saved.")
+//        }
         // Do any additional setup after loading the view, typically from a nib.
+        var video  = PFObject(className: "Videos")
+        println(video)
+        var query = PFQuery(className: "Videos")
+        println(query)
+        movieArray = query.findObjects()!
+        tableView.reloadData();
+        println(movieArray);
+//        query.getObjectInBackgroundWithId(video.objectId) {
+//            (scoreAgain: PFObject!, error: NSError!) -> Void in
+//            if !error {
+//                println("error")
+//            } else {
+//                println("success")
+//            }
+//        var video = query.getObjectInBackgroundWithId("MQfB6Lc3lh")
+        
+        
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+
+    func retrieveVideoFromParse(objects: [PFObject]) {
+        var query = PFQuery(className: "Videos")
+        
+        
+     
+//        for object in objects {
+//            println("found an object")
+//        }
+//        println("hi")
+//        let userVideo = PFObject(className: "Videos")
+//        let userVideoFile = userVideo["video"] as! PFFile
+//        
+//        println(userVideoFile)
+    }
+  
+    /* Table view protocol methods */
+    
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let reuseIdentifier = "cell"
+        var cell:UITableViewCell? = self.tableView.dequeueReusableCellWithIdentifier(reuseIdentifier) as? UITableViewCell
+        if(cell == nil) {
+            cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: reuseIdentifier)
+        }
+        var movie = (self.movieArray[indexPath.row]) as! PFObject
+        println(movie.objectId)
+        println(movie)
+        cell?.textLabel?.text = movie.objectId // movie["objectId"] as! String
+        return cell!;
+    }
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let result = movieArray.count;
+        println("we have \(result) rows")
+        return movieArray.count;
     }
 
 }
