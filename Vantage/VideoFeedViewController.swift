f9ca6ae3a86acbc658ce26292808d2359e0ce535
Lauren N. Roth <laurenroth@gmail.com>
2015-04-19 22:18:11 -0400

    Play video queue from video list. 	modified:   VideoFeedViewController.swift

--- Vantage/VideoFeedViewController.swift
+++ Vantage/VideoFeedViewController.swift
@@ -44,44 +44,38 @@ class VideoFeedViewController: UIViewController, UITableViewDelegate, UITableVie
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         
-        var movieList : [NSURL] = []
-        
-        let onemovie = self.movieArray[0]["video"] as! PFFile
-        
-        println(onemovie)
-        
-        let moviedata = onemovie.url
-        
-        
-        var videoURL = NSURL(string: moviedata!)!
-        
-        
-        
-        let onemovie2 = self.movieArray[11]["video"] as! PFFile
-        
-        let moviedata2 = onemovie2.url
-        
-        
-        var videoURL2 = NSURL(string: moviedata2!)!
-        
-        movieList.append(videoURL)
-        
-        movieList.append(videoURL2)
+//        var movieList : [NSURL] = []
+//        let onemovie = self.movieArray[0]["video"] as! PFFile
+//        
+//        println(onemovie)
+//        
+//        let moviedata = onemovie.url
+//        
+//        
+//        var videoURL = NSURL(string: moviedata!)!
+//        let onemovie2 = self.movieArray[11]["video"] as! PFFile
+//        
+//        let moviedata2 = onemovie2.url
+//        var videoURL2 = NSURL(string: moviedata2!)!
+//        
+//        movieList.append(videoURL)
+//        
+//        movieList.append(videoURL2)
         
         
         let destination = segue.destinationViewController as! AVPlayerViewController
 
-        
-        let secondItem = AVPlayerItem(URL: videoURL2)
-        
-        let firstItem = AVPlayerItem(URL: videoURL)
+//        
+//        let secondItem = AVPlayerItem(URL: videoURL2)
+//        
+//        let firstItem = AVPlayerItem(URL: videoURL)
 
-        
-        println(firstItem)
+//        
+//        println(firstItem)
+//
+//        var movieThing:AnyObject = [firstItem, secondItem]
 
-        var movieThing:AnyObject = [firstItem, secondItem]
-
-        destination.player = AVQueuePlayer(items: movieThing as! [AnyObject])
+//        destination.player = AVQueuePlayer(items: movieThing as! [AnyObject])
         
         
         
@@ -106,39 +100,79 @@ class VideoFeedViewController: UIViewController, UITableViewDelegate, UITableVie
     func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
     }
     
+//    func tableView(tableView: UITableView, editActionsForRowAtIndexPath indexPath: NSIndexPath) -> [AnyObject]?  {
+//        // 1
+//        var recordAction = UITableViewRowAction(style: UITableViewRowActionStyle.Default, title: "REC" , handler: { (action:UITableViewRowAction!, indexPath:NSIndexPath!) -> Void in
+//            // 2
+//            let recordMenu = UIAlertController(title: nil, message: "Add On!", preferredStyle: .ActionSheet)
+//            
+//            let callActionHandler = { (action:UIAlertAction!) -> Void in
+//                var vc = self.storyboard?.instantiateViewControllerWithIdentifier("videoList")as! QueueLoopVideoPlayer
+//                self.presentViewController(vc, animated: true, completion: nil)
+//            }
+//            
+//            let recordAction = UIAlertAction(title: "Record", style: UIAlertActionStyle.Default, handler: nil)
+//            let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: nil)
+//            
+//            recordMenu.addAction(recordAction)
+//            recordMenu.addAction(cancelAction)
+//            
+//            self.presentViewController(recordMenu, animated: true, completion: nil)
+//        })
+//        // 3
+//        var playAction = UITableViewRowAction(style: UITableViewRowActionStyle.Default, title: ">" , handler: { (action:UITableViewRowAction!, indexPath:NSIndexPath!) -> Void in
+//            // 4
+//            let playMenu = UIAlertController(title: nil, message: "Play!", preferredStyle: .ActionSheet)
+//            
+//              let recordAction = UIAlertAction(title: "Record", style: UIAlertActionStyle.Default, handler: callActionHandler)
+//            let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: nil)
+//            
+//            playMenu.addAction(playAction)
+//            playMenu.addAction(cancelAction)
+//            
+//            
+//            self.presentViewController(playMenu, animated: true, completion: nil)
+//        })
+//        // 5
+//        return [recordAction,playAction]
+//    }
+    
     func tableView(tableView: UITableView, editActionsForRowAtIndexPath indexPath: NSIndexPath) -> [AnyObject]?  {
+        
         // 1
         var recordAction = UITableViewRowAction(style: UITableViewRowActionStyle.Default, title: "REC" , handler: { (action:UITableViewRowAction!, indexPath:NSIndexPath!) -> Void in
-            // 2
-            let recordMenu = UIAlertController(title: nil, message: "Add On!", preferredStyle: .ActionSheet)
-            
-            let callActionHandler = {(action:UIAlertAction!) -> Void in
-                var vc = self.storyboard?.instantiateViewControllerWithIdentifier("friendsList")as! FriendsListController
+     
+            let recordMenu = UIAlertController(title: nil, message: "Add on!", preferredStyle: .ActionSheet)
+            let callActionHandler = { (action:UIAlertAction!) -> Void in
+                var vc = self.storyboard?.instantiateViewControllerWithIdentifier("videoList")as! QueueLoopVideoPlayer
                 self.presentViewController(vc, animated: true, completion: nil)
             }
-            
-            let recordAction = IAlertAction(title: "Record", style: UIAlertActionStyle.Default, handler: callActionHandler)
+
+            let recordAction = UIAlertAction(title: "Record", style: UIAlertActionStyle.Default, handler: callActionHandler)
             let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: nil)
-            
+
             recordMenu.addAction(recordAction)
             recordMenu.addAction(cancelAction)
             
             self.presentViewController(recordMenu, animated: true, completion: nil)
-        })
-        // 3
-        var playAction = UITableViewRowAction(style: UITableViewRowActionStyle.Default, title: ">" , handler: { (action:UITableViewRowAction!, indexPath:NSIndexPath!) -> Void in
-            // 4
-            let playMenu = UIAlertController(title: nil, message: "Rate this App", preferredStyle: .ActionSheet)
             
-            let appRateAction = UIAlertAction(title: "Rate", style: UIAlertActionStyle.Default, handler: nil)
+        })
+        
+        // 3
+        
+        var playAction = UITableViewRowAction(style: UITableViewRowActionStyle.Default, title: ">" , handler: { (action:UITableViewRowAction!, indexPath:NSIndexPath!) -> Void in
+            
+            // 4
+            let playMenu = UIAlertController(title: nil, message: "Play!", preferredStyle: .ActionSheet)
+            let playAction = UIAlertAction(title: "Play", style: UIAlertActionStyle.Default, handler: nil)
             let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: nil)
             
-            playMenu.addAction(appRateAction)
+            playMenu.addAction(playAction)
             playMenu.addAction(cancelAction)
-            
             
             self.presentViewController(playMenu, animated: true, completion: nil)
         })
+        
         // 5
         return [recordAction,playAction]
     }
