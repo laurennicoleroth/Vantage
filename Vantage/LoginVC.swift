2a15fb4874b75082156defb3a44efbee9ee7d2ae
Angel Baek <angelbaek1@gmail.com>
2015-04-19 23:47:01 -0400

    user auth segues to dashboard. must customize login settings

--- Vantage/LoginVC.swift
+++ Vantage/LoginVC.swift
@@ -1,44 +1,33 @@
 //
-//  HomeVC.swift
+//  LoginVC.swift
 //  Vantage
 //
-//  Created by Angel Baek on 4/18/15.
+//  Created by Apprentice on 4/19/15.
 //  Copyright (c) 2015 Apprentice. All rights reserved.
 //
 
-//import Foundation
 import UIKit
 import Parse
 import ParseUI
 
-class HomeVC: UIViewController, PFLogInViewControllerDelegate, PFSignUpViewControllerDelegate {
-    
-    @IBOutlet weak var usernameLabel: UILabel!
-    
-    @IBAction func logOutTapped(sender: UIButton) {
-        PFUser.logOut()
-        var currentUser = PFUser.currentUser() //should now be nil
-        self.redirectToLogin()
-    }
+class LoginVC: UIViewController, PFLogInViewControllerDelegate, PFSignUpViewControllerDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
-        
         self.loginSetUp()
+        self.redirectToDashboard()  
     }
-
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
     
     // MARK: - Parse Login
     func logInViewController(logInController: PFLogInViewController!, shouldBeginLogInWithUsername username: String!, password: String) -> Bool {
@@ -48,42 +37,36 @@ class HomeVC: UIViewController, PFLogInViewControllerDelegate, PFSignUpViewContr
             return false
         }
     }
-
+    
     func logInViewController(logInController: PFLogInViewController, didLogInUser user: PFUser) {
-
-        self.dismissViewControllerAnimated(true, completion: nil)
-
-    }
-
-    func logInViewController(logInController: PFLogInViewController!, didFailToLogInWithError error: NSError!) {
-        println("Failed to login...")
-    }
-
-    func logInViewControllerDidCancelLogIn(logInController: PFLogInViewController) {
-
-    }
-
-    // MARK: - Parse Signup
-    func signUpViewController(signUpController: PFSignUpViewController, didSignUpUser user: PFUser) {
-
-        self.dismissViewControllerAnimated(true, completion: nil)
-
-    }
-    func signUpViewController(signUpController: PFSignUpViewController!, didFailToSignUpWithError error: NSError!) {
-
-        println("FAiled to sign up...")
-
-    }
-
-    func signUpViewControllerDidCancelSignUp(signUpController: PFSignUpViewController!) {
         
-        println("User dismissed sign up.")
+        self.dismissViewControllerAnimated(true, completion: nil)
         
     }
     
-    @IBAction func logOutAction(sender: AnyObject) {
-        PFUser.logOut()
-        self.loginSetUp()
+    func logInViewController(logInController: PFLogInViewController!, didFailToLogInWithError error: NSError!) {
+        println("Failed to login...")
+    }
+    
+    func logInViewControllerDidCancelLogIn(logInController: PFLogInViewController) {
+        
+    }
+    
+    // MARK: - Parse Signup
+    func signUpViewController(signUpController: PFSignUpViewController, didSignUpUser user: PFUser) {
+        
+        self.dismissViewControllerAnimated(true, completion: nil)
+        
+    }
+    func signUpViewController(signUpController: PFSignUpViewController!, didFailToSignUpWithError error: NSError!) {
+        
+        println("FAiled to sign up...")
+        
+    }
+    
+    func signUpViewControllerDidCancelSignUp(signUpController: PFSignUpViewController!) {
+        
+        println("User dismissed sign up.")
         
     }
     
@@ -99,17 +82,19 @@ class HomeVC: UIViewController, PFLogInViewControllerDelegate, PFSignUpViewContr
             logInViewController.signUpController = signUpViewController
             self.presentViewController(logInViewController, animated: true, completion: nil)
         }
-
+        
         
     }
     
-    
-    func redirectToLogin() {
-        let vc = self.storyboard?.instantiateViewControllerWithIdentifier("loginView") as! LoginViewController
+    func redirectToDashboard() {
+        let vc = self.storyboard?.instantiateViewControllerWithIdentifier("dashboardi") as! VideoFeedViewController
         self.presentViewController(vc, animated: true, completion: nil)
     }
-
-
-
-
+    
+    @IBAction func logoutAction(sender: AnyObject) {
+        PFUser.logOut()
+        self.loginSetUp()
+    }
+    
+    
 }
